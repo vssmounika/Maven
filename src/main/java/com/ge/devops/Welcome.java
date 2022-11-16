3a93c2d1ada9c9869a1944a95f3b98f36723393d
vssmounika <vssmounika@gmail.com>
2022-11-16 11:12:45 +0530

    first commit

---
+++ src/main/java/com/ge/devops/Welcome.java
@@ -0,0 +1,13 @@
+package com.ge.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class Welcome 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Welcome to my World!" );
+    }
+}
---
+++ src/test/java/com/ge/devops/AppTest.java
@@ -0,0 +1,39 @@
+package com.ge.devops;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+        //return new TestSuite( Welcome.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
